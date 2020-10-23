5a30eab7d428605d70279a4fe68dfe75f394b5dc
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2020-10-23 16:36:23 -0400

    Initial Commit

---
+++ For2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  For2048
+//
+//  Created by Zhaoyang Li on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
